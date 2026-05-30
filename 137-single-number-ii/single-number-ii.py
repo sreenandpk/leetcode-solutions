20811822468ea3e259bb0da83125c45d16ebbfcf
Sreenand P K <sreenand7000@gmail.com>
2026-05-30 11:36:54 +0530

    Time: 3 ms (72.33%) | Memory: 20.9 MB (28.60%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for key,value in d.items():
+            if value==1:
+                return key
+s=Solution()
+print(s.singleNumber(nums = [2,2,3,2]))
