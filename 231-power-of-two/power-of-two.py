f7577355596131fe3c0982f10cf3ddfa2696b2d3
Sreenand P K <sreenand7000@gmail.com>
2026-06-08 09:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (85.51%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        i=0
+        while 2**i<=n:
+            if 2**i==n:
+                return True
+            i+=1
+        return False
+s=Solution()
+print(s.isPowerOfTwo(n = 1))
