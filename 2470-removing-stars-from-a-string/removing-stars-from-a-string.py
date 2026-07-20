beea9d3f27d362a5377fdd58071f992653aa26b2
Sreenand P K <sreenandpk3@gmail.com>
2026-07-20 12:20:10 +0530

    Time: 214 ms (7.18%) | Memory: 20.6 MB (19.21%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack=[]
+        i=0
+        while i<len(s):
+            if s[i]!="*":
+                stack.append(s[i])
+            else:
+                stack.pop()
+            i+=1
+        return "".join(stack)
+s=Solution()
+print(s.removeStars(s = "leet**cod*e"))
